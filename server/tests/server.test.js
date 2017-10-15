86e9d903e1efebdbcb8d3134adc0c536a5c30f26
Oscarlo <oscarlo@Oscarlos-MacBook-Pro.local>
2017-10-15 17:12:10 -0400

    Add PATCH /todos/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -116,12 +118,11 @@ describe('DELETE /todos/:id', () => {
           return done(err);
         }
 
-        // query database using findById to NotExist
         Todo.findById(hexId).then((todo) => {
-          expect(todo).toNotExist()
+          expect(todo).toNotExist();
           done();
-        }).catch((e) => done(e));;
-      })
+        }).catch((e) => done(e));
+      });
   });
 
   it('should return 404 if todo not found', (done) => {
@@ -139,4 +140,47 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+
+    let hexID = todos[0]._id.toHexString();
+    let text = 'This is the new text';
+
+    request(app)
+      .patch(`/todos/${hexID}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  // it('should clear completedAt when todo is not completed', (done) => {
+  //
+  //   let hexID = todos[1]._id.toHexString();
+  //   let text = 'This is the new text!';
+  //
+  //   request(app)
+  //     .patch(`/todos/${hexID}`)
+  //     .send({
+  //       completed: false,
+  //       text
+  //     })
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body.todo.text).toBe(text);
+  //       expect(res.body.todo.completed).toBe(false);
+  //       // Have to revisit since getting "Error: Expected 1508101857590 to not exist"
+  //       // expect(res.body.todo.completedAt).toNotExist();
+  //     })
+  //     .end(done);
+  // });
 });
