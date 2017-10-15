04fc374658136465db6024742ca10986da903678
Oscarlo <oscarlo@Oscarlos-MacBook-Pro.local>
2017-10-15 17:13:50 -0400

    Bug fix for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -163,24 +163,23 @@ describe('PATCH /todos/:id', () => {
       .end(done);
   });
 
-  // it('should clear completedAt when todo is not completed', (done) => {
-  //
-  //   let hexID = todos[1]._id.toHexString();
-  //   let text = 'This is the new text!';
-  //
-  //   request(app)
-  //     .patch(`/todos/${hexID}`)
-  //     .send({
-  //       completed: false,
-  //       text
-  //     })
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.body.todo.text).toBe(text);
-  //       expect(res.body.todo.completed).toBe(false);
-  //       // Have to revisit since getting "Error: Expected 1508101857590 to not exist"
-  //       // expect(res.body.todo.completedAt).toNotExist();
-  //     })
-  //     .end(done);
-  // });
+  it('should clear completedAt when todo is not completed', (done) => {
+
+    let hexID = todos[1]._id.toHexString();
+    let text = 'This is the new text!';
+
+    request(app)
+      .patch(`/todos/${hexID}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
 });
