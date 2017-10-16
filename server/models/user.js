ee1a859093d3de3fde862755fdedf54a53645921
Oscarlo <oscarlo@Oscarlos-MacBook-Pro.local>
2017-10-16 10:06:30 -0400

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,16 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByCredentials = function (email, password) {
   let User = this;
 
