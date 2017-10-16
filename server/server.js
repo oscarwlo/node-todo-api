0e18a1bffad213fb5b33b0af483438ad881b3261
Oscarlo <oscarlo@Oscarlos-MacBook-Pro.local>
2017-10-16 06:54:08 -0400

    Add tests for GET /users/me & POST /users

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const {User} = require('./models/user');
 const {authenticate} = require('./middleware/authenticate');
 
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
-  };
+  }
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
@@ -60,7 +60,7 @@ app.delete('/todos/:id', (req, res) => {
   // validate teh id -> not valid then return 404
   if (!ObjectID.isValid(id)) {
       return res.status(404).send();
-  };
+  }
   // remove todo by id
   Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
@@ -107,7 +107,7 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user)
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
